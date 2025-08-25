a8eb909768e059a98b80bada018300d53df0ce61
sh2624 <sh2624@daum.net>
2025-08-25 17:24:44 +0900

    Refactor: 홈화면 캐릭터카드 수정

--- src/data/imageMap.js
+++ src/data/imageMap.js
@@ -1,13 +1,13 @@
 import bg1 from "../assets/shop/backgrounds/bg1.png";
 import bg2 from "../assets/shop/backgrounds/bg2.png";
 
-import bbiStep1 from "../assets/characters/bbiStep1.png"
-import bbiStep2 from "../assets/characters/bbiStep2.png"
-import bbiStep3 from "../assets/characters/bbiStep3.png"
+import bbiStep1 from "../assets/characters/bbiStep1.png";
+import bbiStep2 from "../assets/characters/bbiStep2.png";
+import bbiStep3 from "../assets/characters/bbiStep3.png";
 
-import catStep1 from "../assets/characters/catStep1.png"
-import catStep2 from "../assets/characters/catStep2.png"
-import catStep3 from "../assets/characters/catStep3.png"
+import catStep1 from "../assets/characters/catStep1.png";
+import catStep2 from "../assets/characters/catStep2.png";
+import catStep3 from "../assets/characters/catStep3.png";
 
 export const BG_MAP = {
   1: bg1,
@@ -18,12 +18,14 @@ export const BG_MAP = {
 export const getBgImg = (backgroundId) => BG_MAP[backgroundId] ?? bg1;
 
 export const CHARACTER_MAP = {
-  1: { // 캐릭터 ID 1 = 삐약이
+  1: {
+    // 캐릭터 ID 1 = 삐약이
     1: { img: bbiStep1, title: "호기심 많은 삐약이" },
     2: { img: bbiStep2, title: "활발한 삐약이" },
     3: { img: bbiStep3, title: "용맹한 삐약대장" },
   },
-  3: { // 캐릭터 ID 2 = 고양이
+  2: {
+    // 캐릭터 ID 2 = 고양이
     1: { img: catStep1, title: "얌전한 야옹이" },
     2: { img: catStep2, title: "새침한 야옹이" },
     3: { img: catStep3, title: "도도한 야옹이" },
